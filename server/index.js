1f16eb222278c26562dd5af8014a0c6376638c34
Diego Moreno <diegomoreno@cyandark.com>
2017-12-13 20:49:22 -0500

    Servidor con Express montado

--- server/index.js
+++ server/index.js
@@ -3,4 +3,10 @@ var express = require('express');
 // Llamamos al método express
 var app = express();
 // Cargamos el server http pasándole la app de express
-var server = require('http').server(app);
+var server = require('http').Server(app);
+// Pasar la libreria de socket.io para trabajarlos
+var io = require('socket.io')(server);
+
+server.listen(6677, function () {
+    console.log('El server está funcionando en http://localhost:6677!');
+});
